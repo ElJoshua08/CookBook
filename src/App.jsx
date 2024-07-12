d2ca60a676e81373e4b4b5cfcd402d42a52305b4
Joshua <josuealejandrof926@gmail.com>
2024-07-13 01:36:49 +0200

    Add First Link

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from '@/pages/Home';
-import Header from '@/components/Header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from '@/pages/Home'
+import Header from '@/components/Header'
 
 function App() {
   return (
-    <>
-      <Header />
-
+    <div className="App">
         <Router>
+          <Header />
           <Routes>
-            <Route
-            path="/"
-            element={<Home />}
-            />
-          <Route
-            path="/about"
-            element={<h1>About</h1>}
-            />
-          <Route
-            path="/contact"
-            element={<h1>Contact</h1>}
-            />
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<h1>About</h1>} />
+            <Route path="/contact" element={<h1>Contact</h1>} />
           </Routes>
-
         </Router>
-        </> 
-  );
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,42 @@
+import { FaHeart, FaHome } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div className="absolute ml-5 mt-5 bg-zinc-700 min-w-[calc(100%-40px)] py-5 rounded-md shadow-md shadow-zinc-700/50">
-      {/*  */}
-    </div>
-  );
-};
+    <div className="absolute ml-5 mt-5 flex max-h-16 min-w-[calc(100%-40px)] items-center justify-between gap-5 rounded-md bg-zinc-700 py-3 shadow-md shadow-zinc-700/50">
+      {/* logo and title */}
+      <div className="flex h-full items-center justify-start gap-2 pl-2">
+        <img
+          src="/logo/512.png"
+          alt="logo"
+          className="aspect-square w-6 rounded-full"
+        />
+        <h1 className="font-lobster text-lg font-bold text-white">Cookbook</h1>
+      </div>
 
-export default Header;
+      {/* Navigation */}
+      <Nav />
+    </div>
+  )
+}
+
+const Nav = () => {
+  const links = [
+    { name: 'Home', href: '/', icon: <FaHome /> },
+    { name: 'Favorites', href: '/favorites', icon: <FaHeart /> },
+  ]
+
+  console.log(links.map(link => link.name))
+
+  return (
+    <ul className="flex items-center justify-between gap-5">
+      {/* {links.map((link, index) => (
+        <li key={index} className="flex items-center gap-2">
+        </li>
+      ))} */}
+      <Link to="/">Home</Link>
+    </ul>
+  )
+}
+
+export default Header
